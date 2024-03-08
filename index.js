adb6ecd9240da5c29ac06ca9911d574cddc33098
Tridesh <tridesh2005@gmail.com>
2024-03-08 12:32:50 +0530

    some update #1

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const colours = ["pink", "cyan", "green", "blue", "yellow", "violet", "brown", "orange"];
-    var boxes = document.querySelectorAll(".flippable");
-    boxes.forEach(function (box) {
-        box.addEventListener("click", function () {
-            // Toggle the 'flipped' class to trigger the animation
-            box.classList.toggle("flipped");
-            // Add logic here to handle the color assignment (random or predefined)
-        });
-    });
-    for (let i = 1; i < 25; i++) {
-        var box = document.querySelector(`#box${i}`);
-        box.style.backgroundColor = colours[parseInt(Math.random() * 7)];
-        // box.style.backgroundColor = "transparent";
+    const matches = 0, flips = 0;
+    const colors = ["green", "red", "blue", "cyan", "yellow", "violet", "aqua", "brown"];
+    function shuffle(a) {
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+      }
+    shuffle(colors);
+    // console.log(colors);
+    function colorAssignment() {
+        for (let i = 1; i < 17; i++) {
+            var box = document.querySelector(`#box${i}`);
+            i <= 8 ? box.style.backgroundColor = colors[i-1] : box.style.backgroundColor = colors[16-i];
+        }
     }
+    function checkColor(box1, box2) {
+        var colorBoo;
+        box1.style.backgroundColor == box2.style.backgroundColor ? colorBoo = 1 : colorBoo = 0;
+    }
+    
 });
 
 
